683eca8b62b62fbfac53d71ea362dfd9fc031d37
premagikwad1213 <a70064182@gmail.com>
2026-08-01 23:58:01 +0530

    new update

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+'use-client'
+
+import {MainStudioModel} from "@/components/MainStudioModel"
+import {View} from "@react-three/drei"
+import { useState } from "react"
+import {useMediaQuery} from "react-responsive"
+
+export default  function Home () {
+
+}
