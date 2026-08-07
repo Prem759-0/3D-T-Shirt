41e81639f86c375268963b832669d13a4b600493
premagikwad1213 <a70064182@gmail.com>
2026-08-08 03:46:03 +0530

    update

---
+++ components/ViewCanvas.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Ring, View } from "@react-three/drei";
+
+import AssetsPreload from "./AssetsPreload";
+import Rig from "./Rig";
+import LoadingSkeleton from "./LoadingSkeleton";
+import { patchThreeLoadingManager } from "@/lib/patchThreeLoadingManager";
+patchThreeLoadingManager();
+
+const ViewCanvas = () => {
+  const [eventSource, setEventSource] = useState<HTMLElement | null>(null);
+  useEffect(() => {
+    setEventSource(document.body);
+  }, []);
+  return(
+    <Canvas
+      style={{position: "fixed", inset: 0, overflow: "hidden"}}
+      camera={{position: [0, 0, 7.3], fov: 30}}
+      eventSource={eventSource ?? undefined}
+      eventPrefix="client"
+      gl={{stencil: true}}>
+       <AssetsPreload/>
+       <Suspense fallback={<LoadingSkeleton/>}>
+        <View.Port/>
+       </Suspense>
+       <Rig/>
+    </Canvas>
+  )
+}
+
+export default ViewCanvas;
